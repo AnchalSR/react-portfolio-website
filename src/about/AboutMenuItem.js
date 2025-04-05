edbead1436bff93375f759cb9c681e8a48fe59d6
Anchal <147301242+AnchalSR@users.noreply.github.com>
2025-04-06 00:24:48 +0530

    Update AboutMenuItem.js

--- src/about/AboutMenuItem.js
+++ src/about/AboutMenuItem.js
@@ -1,10 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import classNames from "classnames";
 import "../styles/aboutMenu.css";
 
 const AboutMenuItem = ({ title, active, onClick }) => {
   return (
-    <div className={classNames("item", { active })} onClick={onClick}>
+    <div
+      className={classNames("item", { active })}
+      onClick={onClick}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          onClick();
+        }
+      }}
+    >
       <h2 className="title">{title}</h2>
     </div>
   );
